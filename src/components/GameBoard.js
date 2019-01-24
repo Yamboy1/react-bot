19f34b3f36dfbff97e4328305c7a40e322ce950b
Yamboy1 <16280@grammar.net.nz>
2019-01-25 06:42:51 +1300

    Render board

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import Emoji from './Emoji';
 
 class GameBoard extends Component {
     render() {
-        return this.props.matrix.map(row => row.join(' ')).join('\n');
+        return this.props.matrix.map((row, rowIndex) => 
+            row.map((item,itemIndex) => 
+                <Emoji name={"c4_"+item} key={rowIndex+" "+itemIndex} />).concat("\n"));
     }
 }
 
